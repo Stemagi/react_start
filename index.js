c1614bd485d75a11f346756cf00554bc1fe9888b
stm <stm2485@gmail.com>
2023-03-07 13:38:47 +0200

    Render function

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-ReactDOM.render(<p1>Hello everyone this is my first react project</p1>, document.querySelector("#root"))
+function MainContent() {
+  return(
+    <h1>
+      Im learning React!
+    </h1>
+  )
+}
 ReactDOM.render(
-  <ul><li>Thing1</li><li>Thing2</li></ul>,
-  document.querySelector("#ul")
-)
-ReactDOM.render(<div>
-  <h1>
-    Hello everyone
-  </h1>
-  <p>
-    This is my practice
-  </p>
+  <div>
+    <MainContent />
   </div>,
-  document.getElementById("test")
+  document.querySelector("#root")
 )
