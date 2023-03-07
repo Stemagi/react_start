fa48412c3baf77f1f7932218e2061478b71296b1
stm <stm2485@gmail.com>
2023-03-07 12:52:35 +0200

    just_practice

---
+++ index.js
@@ -0,0 +1,15 @@
+ReactDOM.render(<p1>Hello everyone this is my first react project</p1>, document.querySelector("#root"))
+ReactDOM.render(
+  <ul><li>Thing1</li><li>Thing2</li></ul>,
+  document.querySelector("#ul")
+)
+ReactDOM.render(<div>
+  <h1>
+    Hello everyone
+  </h1>
+  <p>
+    This is my practice
+  </p>
+  </div>,
+  document.getElementById("test")
+)
