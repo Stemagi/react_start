27385893b99895443e651216595ebd400df1ec0c
stm <stm2485@gmail.com>
2023-03-08 12:45:13 +0200

    custom components part 1

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-const page= (
+// const mypage = (
+//        <div>
+//             <h1>Why Im exited to be learning React</h1>
+//             <ul>
+//                 <li>poh</li>
+//                 <li>hz che</li>
+//                 <li>wanna work and learn money</li>
+//             </ul>
+//         </div>
+// )
+// dont work
+// // Function Mypage()=
+// //     return(
+// //         <div>
+// //             <h1>Why Im exited to be learning React</h1>
+// //             <ul>
+// //                 <li>poh</li>
+// //                 <li>hz che</li>
+// //                 <li>wanna work and learn money</li>
+// //             </ul>
+// //         </div>
+// //     )
+//     ReactDom.render(mypage, document.getElementById("root"))
+// const mypage= (
+//   <div>
+//     <h1>Why im exited to be learning react</h1>
+//     <ul>
+//       <li>chetotam</li>
+//       <li>wanna work</li>
+//     </ul>
+//     <p>emmet10</p>
+//   </div>
+// )
+function Mypage() {
+  return(
     <div>
-    <img src="./react-logo.png" width="40px" />
-    <h1>Fun Facts about React</h1>
-    <ul>
-        <li>was realeased in 2013</li>
-        <li>was originaly</li>
-        <li>choto tam</li>
-        <li>i vashe top</li>
-        <li>veselo uchit</li>
-    </ul>
-    </div>
-)
-ReactDOM.render(page, document.getElementById("root"))
+    <h1>Why im exited to be learning react</h1>
+    <ol>
+      <li>chetotam</li>
+      <li>wanna work</li>
+    </ol>
+    <p>emmet10</p>
+  </div>
+  )
+}
+  
+
+ReactDOM.render(<Mypage />, document.querySelector("#root"))
