fb612e202e7c5421f8a73a971ac1e9e999b74ee4
stm <stm2485@gmail.com>
2023-03-08 12:57:19 +0200

    second part of challenge

--- index.js
+++ index.js
@@ -1,45 +1,17 @@
-// const mypage = (
-//        <div>
-//             <h1>Why Im exited to be learning React</h1>
-//             <ul>
-//                 <li>poh</li>
-//                 <li>hz che</li>
-//                 <li>wanna work and learn money</li>
-//             </ul>
-//         </div>
-// )
-// dont work
-// // Function Mypage()=
-// //     return(
-// //         <div>
-// //             <h1>Why Im exited to be learning React</h1>
-// //             <ul>
-// //                 <li>poh</li>
-// //                 <li>hz che</li>
-// //                 <li>wanna work and learn money</li>
-// //             </ul>
-// //         </div>
-// //     )
-//     ReactDom.render(mypage, document.getElementById("root"))
-// const mypage= (
-//   <div>
-//     <h1>Why im exited to be learning react</h1>
-//     <ul>
-//       <li>chetotam</li>
-//       <li>wanna work</li>
-//     </ul>
-//     <p>emmet10</p>
-//   </div>
-// )
 function Mypage() {
   return(
     <div>
     <h1>Why im exited to be learning react</h1>
+    <h2>
+      <nav>
+        <img src="./react-logo.png" width="70px"/>
+      </nav>
+    </h2>
     <ol>
       <li>chetotam</li>
       <li>wanna work</li>
     </ol>
-    <p>emmet10</p>
+    <p>© 20xx Krivko development. All rights reserved.</p>
   </div>
   )
 }
