be4f9feea536c7b17c107a099647dceb61326f95
stm <stm2485@gmail.com>
2023-03-08 13:20:59 +0200

    parent and child components

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
-function Mypage() {
+function Header() {
+  return(
+    <header>
+    <nav>
+      <img src="./react-logo.png" width="70px"/>
+    </nav>
+  </header>
+  )
+}
+function MainContent() {
   return(
     <div>
-       <header>
-      <nav>
-        <img src="./react-logo.png" width="70px"/>
-      </nav>
-    </header>
     <h1>Why im exited to be learning react</h1>
     <ol>
       <li>chetotam</li>
       <li>wanna work</li>
     </ol>
-    <footer>
-      <small>© 20xx Krivko development. All rights reserved.</small>
-      </footer>
-  </div>
+    </div>
+  )
+
+}
+function Footer() {
+  return(
+  <footer>
+  <small>© 20xx Krivko development. All rights reserved.</small>
+  </footer>
   )
 }
   
+function Mypage() {
+  return(
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+  </div>
+  )
+}
+
 
 ReactDOM.render(<Mypage />, document.querySelector("#root"))
+// ReactDOM.render(<Header />, document.querySelector("#root"))
