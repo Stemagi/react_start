ba2fb53d608b696ea1c6219211f68200a47fa48e
stm <stm2485@gmail.com>
2023-03-08 13:32:51 +0200

    lil popravki

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
 function Header() {
-  return(
+  return (
     <header>
-    <nav>
-      <img src="./react-logo.png" width="70px"/>
-    </nav>
-  </header>
-  )
+      <nav>
+        <img src="./react-logo.png" width="70px" />
+      </nav>
+    </header>
+  );
 }
 function MainContent() {
-  return(
+  return (
     <div>
-    <h1>Why im exited to be learning react</h1>
-    <ol>
-      <li>chetotam</li>
-      <li>wanna work</li>
-    </ol>
+      <h1>Why im exited to be learning react</h1>
+      <ol>
+        <li>chetotam</li>
+        <li>wanna work</li>
+      </ol>
     </div>
-  )
-
+  );
 }
 function Footer() {
-  return(
-  <footer>
-  <small>© 20xx Krivko development. All rights reserved.</small>
-  </footer>
-  )
+  return (
+    <footer>
+      <small>© 20xx Krivko development. All rights reserved.</small>
+    </footer>
+  );
 }
-  
+
 function Mypage() {
-  return(
+  return (
     <div>
       <Header />
       <MainContent />
       <Footer />
-  </div>
-  )
+    </div>
+  );
 }
 
-
-ReactDOM.render(<Mypage />, document.querySelector("#root"))
+ReactDOM.render(<Mypage />, document.querySelector("#root"));
 // ReactDOM.render(<Header />, document.querySelector("#root"))
